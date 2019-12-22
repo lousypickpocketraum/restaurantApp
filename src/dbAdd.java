666676b9f3dec953708f750615cd1b48677015dd
akin <akin.tekeoglu@gmail.com>
2019-12-22 20:18:16 +0300

    mysql driver eklendi

--- src/dbAdd.java
+++ src/dbAdd.java
@@ -5,11 +5,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.beans.Statement;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 
-@WebServlet(name="dbAdd")
+@WebServlet(name="dbAdd",urlPatterns = "/dbAdd")
 public class dbAdd extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
@@ -17,19 +18,25 @@ public class dbAdd extends HttpServlet {
         String yemekUcretiEkle=request.getParameter("yemekUcreti");
         double yemekUcretiEkle2 = Double.valueOf(yemekUcretiEkle);
         PreparedStatement stat = null;
+        
         try {
-            stat = (PreparedStatement) dbConnection.getConnection().createStatement();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        String add = "INSERT INTO menu(yemekadi , yemekucreti)"
-                + "VALUES (" + "'" + yemekAdiEkle + "'" +"," + yemekUcretiEkle2 + ")";
-        try{
-            stat.executeUpdate(add);
-        }catch (Exception e){
-            System.out.println(e);
-        }
-
+        	 Connection connection = dbConnection.getConnection();
+        	 String add = "INSERT INTO menu(yemekadi , yemekucreti)"
+  	                + "VALUES (" + "'" + yemekAdiEkle + "'" +"," + yemekUcretiEkle2 + ")";
+ 			 stat = connection.prepareStatement(add);
+ 			 stat.executeUpdate(add);
+		} catch (SQLException| ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(stat!=null)
+					stat.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+        
 
     }
 }
--- src/dbConnection.java
+++ src/dbConnection.java
@@ -1,7 +1,8 @@
 import java.sql.*;
 
 public class dbConnection {
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+    	Class.forName("com.mysql.jdbc.Driver");  
         String host = "jdbc:mysql://localhost:3306/restraurant?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey";
         String userName = "root";
         String userPass = "1234";
